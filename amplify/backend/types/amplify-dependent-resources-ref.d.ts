bae99feda7e182108c24d4cfd74533ad4fdc193d
epolga <olga.epstein@gmail.com>
2025-04-26 10:07:07 +0300

    after amplify deploy

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,20 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "crossStitchApi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "allDesignsApiLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
   "storage": {
     "CrossStitchItems": {
       "Arn": "string",
