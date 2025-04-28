a4b455bbcd58ff5bce0ebd540cc34a845aef2409
epolga <olga.epstein@gmail.com>
2025-04-28 20:27:43 +0300

    Test all designs

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ interface Design {
     Text: string;
     NPage: number;
 }
-
+/*
 export default function Home() {
     const [tableName, setTableName] = useState<string>("");
 
@@ -40,6 +40,53 @@ export default function Home() {
     return (
         <div className={styles.container}>
             <h1>DynamoDB Table: {tableName}</h1>
+        </div>
+    );
+}
+*/
+
+export default function Home() {
+    const [designs, setDesigns] = useState<Design[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    useEffect(() => {
+        async function fetchDesigns() {
+            try {
+               const res = await fetch("/api/all-designs");
+               if (!res.ok) throw new Error("Failed to fetch designs");
+               const designs = await res.json();
+
+               setDesigns(designs);
+               console.log(designs);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchDesigns();
+    }, []);
+
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Latest Designs</h1>
+            {isLoading ? (
+                <p>Loading designs...</p>
+            ) : designs.length === 0 ? (
+                <p>No designs found.</p>
+            ) : (
+                <div className={styles.designGrid}>
+                    {designs.map((design) => (
+                        <div key={design.DesignID} className={styles.designCard}>
+                            <h3 className={styles.DesignCaption}>{design.Caption}</h3>
+                            <p>{design.Description}</p>
+                            <p>Downloads: {design.NDownloaded}</p>
+                            <p>Size: {design.Width}x{design.Height}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
